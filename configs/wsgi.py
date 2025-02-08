20b7a7f434832ab28932d27902ecf4769d409f65
lemajr <erickbale360@gmail.com>
2025-02-09 00:35:22 +0300

    fix typo

--- configs/wsgi.py
+++ configs/wsgi.py
@@ -10,9 +10,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'configs.settings')
 
 application = get_wsgi_application()
 
 app = application
+
+
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
